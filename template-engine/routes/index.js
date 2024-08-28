fbc63618976036350a70d89f631abd86d4b53e28
German Koch <gjkoch7@gmail.com>
2024-08-27 23:45:27 -0300

    Creación de layout equipo Arsenal

--- template-engine/routes/index.js
+++ template-engine/routes/index.js
@@ -3,56 +3,69 @@ const router = Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const json_books = fs.readFileSync('./books.json', 'utf-8');
-let books = JSON.parse(json_books);
-
 const json_equipos = fs.readFileSync('./data/equipos.json');
 let equipos = JSON.parse(json_equipos);
+
 
 router.get('/', (req, res) => {
     res.render('home_index', {
         layout: 'index',
         data: {
             equipos,
-            books
         }
     });
 });
 
-router.get('/new-entry', (req, res) => {
-    res.render('home_new-entry', {
-        layout: 'new-entry',
+router.get('/new-team', (req, res) => {
+    res.render('home_new-team', {
+        layout: 'new-team',
     });
 });
 
-router.post('/new-entry', (req, res) => {
-    const {title, author, image, description} = req.body;
-    if (!title || !author || !image || !description) {
-        res.status(400).send('Entries must have a title and description');
+router.post('/new-team', (req, res) => {
+    const { name, area, crestUrl, clubColors } = req.body;
+    if (!name || !area || !crestUrl || !clubColors) {
+        res.status(400).send('Entrie must a team name and country');
         return;
     }
 
-    let newBook = {
+    let newEquipos = {
         id: uuidv4(),
-        title,
-        author,
-        image,
-        description
+        area: { area },
+        name,
+        crestUrl,
+        clubColors
     };
 
-    books.push(newBook);
+    equipos.push(newEquipos);
 
-    const json_books = JSON.stringify(books)
-    fs.writeFileSync('./books.json', json_books, 'utf-8');
-
+    const json_equipos = JSON.stringify(equipos)
+    fs.writeFileSync('./data/equipos.json', json_equipos, 'utf-8');
     res.redirect('/');
 });
 
 router.get('/delete/:id', (req, res) => {
-    books = books.filter(book => book.id != req.params.id);
-    const json_books = JSON.stringify(books)
-    fs.writeFileSync('./books.json', json_books, 'utf-8');
+    equipos = equipos.filter(equipo => equipo.id != req.params.id);
+    const json_equipos = JSON.stringify(equipos)
+    fs.writeFileSync('./data/equipos.json', json_equipos, 'utf-8');
     res.redirect('/');
 });
+
+
+
+router.get('/team57', (req, res) => {
+    const json_equipo = fs.readFileSync('./data/equipos/ARS.json');
+    let equipo = JSON.parse(json_equipo);
+    res.render('home_team-57', {
+        layout: 'team-57',
+        data: {
+            equipo,
+        }
+    });
+});
+
+
+
+router.get('/')
 
 module.exports = router;
